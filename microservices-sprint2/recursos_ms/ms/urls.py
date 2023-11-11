dba894f4f8153ea6947b79aca1de2fb840721d2a
rooot <miguelagc@outlook.es>
2023-11-10 21:28:53 -0500

    +

--- microservices-sprint2/recursos_ms/ms/urls.py
+++ microservices-sprint2/recursos_ms/ms/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('crear_recurso/', views.crear_recurso, name='crear_recurso'),
-    path('buscar_recurso/<int:recurso_id>/', views.buscar_recurso, name='buscar_recurso'),
-    path('recursos/', views.recursos, name='recursos'),
-    
+    path('darmensaje/', views.darmensaje, name='darmensaje'),
 ]
--- microservices-sprint2/recursos_ms/ms/views.py
+++ microservices-sprint2/recursos_ms/ms/views.py
@@ -5,26 +5,5 @@ from .models import ItemInventario
 from django.views.decorators.csrf import csrf_exempt
 
 
-@csrf_exempt
-def crear_recurso(request):
-    if request.method == "POST":
-        data = json.loads(request.body.decode('utf-8'))
-        nombre = data.get('nombre')
-        cantidad = data.get('cantidad')
-
-        if nombre and cantidad is not None:
-            recurso = Recurso(nombre=nombre, cantidad=cantidad)
-            recurso.save()
-            return JsonResponse({"message": "Recurso creado con éxito", "id": recurso.id}, status=201)
-        else:
-            return JsonResponse({"error": "Datos inválidos"}, status=400)
-
-    return JsonResponse({"error": "Método no permitido"}, status=405)
-
-def buscar_recurso(request, recurso_id):
-    recurso = get_object_or_404(Recurso, id=recurso_id)
-    return render(request, 'detalle_recurso.html', {'recurso': recurso})
-
-def recursos(request):
-    lista_recursos = Recurso.objects.all()
-    return render(request, 'lista_recursos.html', {'recursos': lista_recursos})
+def darmensaje(request):
+    return JsonResponse({'Servicio no disponible': 'error 500'}, status=200)
